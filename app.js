f7dc370159637600ecbd0fb7b4a71b92f66eb66e
kfwang177880 <kfwang177880@yahoo.com>
2020-08-13 13:25:20 -0400

    fix syntax

--- app.js
+++ app.js
@@ -2,26 +2,30 @@ const express = require("express");
 const morgan = require("morgan");
 const postBank = require("./postBank");
 
+const PORT = 1337
 const app = express();
 
 app.use(morgain('dev'));
 
 app.get("/", (req, res) => {
   const posts = postBank.list();
-  );
 
-  <!DOCTYPE html>
+  const html = `<!DOCTYPE html>
   <html>
     <head>
       <title>Wizard News</title>
     </head>
     <body>
       <ul>
-        ${posts.map(post => <li> ${/* postdata here */</li>)}
+        ${posts.map(post => `<li> ${/* postdata here */}</li>`)}
       </ul>
     <body>
-  </html>
+  </html>`
 
-  res.send(html)
+// send response
+  res.send(html);
   
+  app.listen(PORT, () => (
+    console.log('App listning in ort ${PORT}');
+  ))
 });
